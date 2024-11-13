2faf64279ad86b615998c96439cf2904c57ae142
Samir <abakhri.sd@gmail.com>
2024-11-13 19:35:58 +0500

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,64 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {createRouter, createWebHistory} from 'vue-router';
-import TheForm from "@/components/form/TheForm.vue";
-import SurveyLayout from "@/components/work-w-back/SurveyLayout.vue";
-import FriendsList from "@/components/FriendsList.vue";
-import NotFound from "@/components/nav/NotFound.vue";
-import FormFooter from "@/components/form/FormFooter.vue";
-import BackFooter from "@/components/work-w-back/BackFooter.vue";
+import router from "@/router.js";
 
-const router = createRouter({
-    routes: [
-        {
-            path: '/',
-            redirect: '/form',
-        },
-        {
-            // alias: '/',
-            path: '/form',
-            components: {
-                default: TheForm,
-                footer: FormFooter,
-            },
-        }, {
-            path: '/back',
-            components: {
-                default: SurveyLayout,
-                footer: BackFooter,
-            },
-            children: [
-                {
-                    name: 'back-friends',
-                    path: 'friends',
-                    component: FriendsList,
-                }
-            ]
-        }, {
-            path: '/:catchAll(.*)',
-            component: NotFound,
-        }
-    ],
-    history: createWebHistory(),
-    linkActiveClass: 'active',
-    scrollBehavior: (to, from, savedPosition) => {
-        if (savedPosition) {
-            return savedPosition;
-        }
-
-        return {
-            left: 0,
-            top: 0,
-        }
-    },
-});
-
-router.beforeEach((to, from, next) => {
-    console.log('router',{to, from});
-    next();
-    // next(false);
-    // next({name: 'my-route-name', params: {...}});
-})
 const app = createApp(App);
 
 app.use(router);
---
+++ src/router.js
@@ -0,0 +1,68 @@
+import {createRouter, createWebHistory} from "vue-router";
+import TheForm from "@/components/form/TheForm.vue";
+import FormFooter from "@/components/form/FormFooter.vue";
+import SurveyLayout from "@/components/work-w-back/SurveyLayout.vue";
+import BackFooter from "@/components/work-w-back/BackFooter.vue";
+import FriendsList from "@/components/FriendsList.vue";
+import NotFound from "@/components/nav/NotFound.vue";
+
+const router = createRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: '/form',
+        },
+        {
+            // alias: '/',
+            path: '/form',
+            meta: {needsAuth: true},
+            components: {
+                default: TheForm,
+                footer: FormFooter,
+            },
+        }, {
+            path: '/back',
+            components: {
+                default: SurveyLayout,
+                footer: BackFooter,
+            },
+            children: [
+                {
+                    name: 'back-friends',
+                    path: 'friends',
+                    component: FriendsList,
+                }
+            ]
+        }, {
+            path: '/:catchAll(.*)',
+            component: NotFound,
+        }
+    ],
+    history: createWebHistory(),
+    linkActiveClass: 'active',
+    scrollBehavior: (to, from, savedPosition) => {
+        if (savedPosition) {
+            return savedPosition;
+        }
+
+        return {
+            left: 0,
+            top: 0,
+        }
+    },
+});
+
+router.beforeEach((to, from, next) => {
+    console.log('router',{to, from});
+    if (to.meta.needsAuth) {
+        console.log('need auth!',)
+        // next(false);
+        next();
+    } else {
+        next();
+    }
+    // next(false);
+    // next({name: 'my-route-name', params: {...}});
+});
+
+export default router;
