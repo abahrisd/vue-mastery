512ba7ea1fa48025a2736aa41170a461d68441ea
Samir <abakhri.sd@gmail.com>
2024-11-18 19:29:18 +0500

    transition mode

--- src/components/animations/AnimationsLayout.vue
+++ src/components/animations/AnimationsLayout.vue
@@ -10,6 +10,12 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsrs" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsrs" v-else>Hide Users</button>
+    </transition>
+  </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -29,6 +35,7 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
@@ -43,6 +50,12 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    showUsrs() {
+      this.usersAreVisible = true;
+    },
+    hideUsrs() {
+      this.usersAreVisible = false;
     },
   },
 };
@@ -123,6 +136,21 @@ button:active {
 .v-leave-to {
   /*opacity: 0;
   transform: translateY(-30px);*/
+}
+
+.fade-button-enter-from, .fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-to, .fade-button-leave-from {
+  opacity: 1;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-out;
 }
 
 @keyframes slide-scale {
