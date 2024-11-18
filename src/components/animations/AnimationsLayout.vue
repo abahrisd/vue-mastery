4a107170c14d380ec0e2fb5e7ee9739c0907a5f7
Samir <abakhri.sd@gmail.com>
2024-11-18 15:38:51 +0500

    keyframes animation

--- src/components/animations/AnimationsLayout.vue
+++ src/components/animations/AnimationsLayout.vue
@@ -67,7 +67,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform 0.3s ease;
+  /*transition: transform 0.3s ease;*/
 }
 .container {
   max-width: 40rem;
@@ -81,6 +81,20 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-150px);
+  /*transform: translateX(-150px);*/
+  animation: slide-fade 0.3s ease forwards;
 }
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+
 </style>
